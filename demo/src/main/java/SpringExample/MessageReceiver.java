5eae2d6d4886c38c708e71a45d5d0bf978f3391f
RAM0N_LUIS <ramonlrodriguez@uchicago.edu>
2016-08-12 23:00:05 -0500

    spring example

---
+++ demo/src/main/java/SpringExample/MessageReceiver.java
@@ -0,0 +1,38 @@
+package SpringExample;
+
+import java.io.File;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileSystemUtils;
+
+
+
+/**
+ * Created by RAM0N on 8/12/16.
+ */
+@Component
+public class MessageReceiver {
+
+    private static final String QUEUE_NAME = "DON'T MIND";
+
+    /**
+     * Get a copy of the application context
+     */
+    @Autowired
+    ConfigurableApplicationContext context;
+
+    /**
+     * When you receive a message, print it out, then shut down the application.
+     * Finally, clean up any ActiveMQ server stuff.
+     */
+    @JmsListener(destination = QUEUE_NAME, containerFactory = "myJmsContainerFactory")
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        context.close();
+        FileSystemUtils.deleteRecursively(new File("activemq-data"));
+    }
+
+
+}
---
+++ demo/src/main/java/activeMQExample/MainDriver.java
@@ -0,0 +1,35 @@
+package activeMQExample;
+
+/**
+ * Hello world!
+ */
+public class MainDriver {
+
+    public static void main(String[] args) throws Exception {
+
+        String sMessage1 = "Hola, Como estas?";
+        String sMessage2 = "Konnichiwa";
+        String sMessage3 = "Pardon my French";
+        String sMessage4 = "Sak pase";
+
+        // send messages on different threads
+        thread(new MessageProducer(sMessage1), false);
+        thread(new MessageProducer(sMessage2), false);
+        thread(new MessageProducer(sMessage3), false);
+        thread(new MessageProducer(sMessage4), false);
+
+        // receive messages on different threads
+        thread(new MessageConsumer(), false);
+        thread(new MessageConsumer(), false);
+        thread(new MessageConsumer(), false);
+        thread(new MessageConsumer(), false);
+    }
+
+
+    public static void thread(Runnable runnable, boolean daemon) {
+        Thread brokerThread = new Thread(runnable);
+        brokerThread.setDaemon(daemon);
+        brokerThread.start();
+    }
+
+}
